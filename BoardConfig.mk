a31d02345c0c53bf3209e498956e2505994e4889
Naufal Altaf <me@altaf.xyz>
2025-02-12 17:38:26 +0700

    RMX1971: Configure kernel, boot and DTBO image configuratons
    Rely on prebuilt kernel for now as it is not yet ready to be compiled inline.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,10 +42,20 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 TARGET_ENABLE_MEDIADRM_64 := true
 
 # Kernel
-TARGET_NO_KERNEL := true
+BOARD_BOOT_HEADER_VERSION := 1
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 swiotlb=1 loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)-kernel/dtbo.img
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)-kernel/$(BOARD_KERNEL_IMAGE_NAME)
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
 
 BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
